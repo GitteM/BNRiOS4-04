be60973b6b8f0f4759e067d2dd5e31fc15e8e25c
Brigitte Michau <brigitte@fontera.com>
2014-08-05 09:33:56 +0200

    Custom Drawing in drawRect

--- Hypnosister/BNRAppDelegate.m
+++ Hypnosister/BNRAppDelegate.m
@@ -16,19 +16,12 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
-    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
+    // Use the bounds property of the window to define the frame for a full-screen instance of BNRHypnosisView
+    CGRect firstFrame = self.window.bounds;
     
     BNRHypnosisView *firstView = [[BNRHypnosisView alloc]initWithFrame:firstFrame];
-    firstView.backgroundColor = [UIColor redColor];
     
     [self.window addSubview:firstView];
-    
-    
-    CGRect secondFrame = CGRectMake(20, 30, 50, 50);
-    BNRHypnosisView *secondView = [[BNRHypnosisView alloc]initWithFrame:secondFrame];
-    secondView.backgroundColor = [UIColor blueColor];
-    
-    [firstView addSubview:secondView];
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
