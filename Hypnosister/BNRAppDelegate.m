986f9a521085e7a9cbba3708b8b6659dd7e3a773
Brigitte Michau <brigitte@fontera.com>
2014-08-09 12:47:02 +0200

    Add an instance of UIScrollView . Put a super sized version of BNRHypnosisView and add the Scrollview to the window.

--- Hypnosister/BNRAppDelegate.m
+++ Hypnosister/BNRAppDelegate.m
@@ -16,12 +16,22 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     
-    // Use the bounds property of the window to define the frame for a full-screen instance of BNRHypnosisView
-    CGRect firstFrame = self.window.bounds;
+    // Create CGRects for frames
+    CGRect screenRect = self.window.bounds;
+    CGRect bigRect = screenRect;
+    bigRect.size.width *= 2.0;
+    bigRect.size.height *= 2.0;
     
-    BNRHypnosisView *firstView = [[BNRHypnosisView alloc]initWithFrame:firstFrame];
+    // Create a screen-sized scroll view and add it to the window
+    UIScrollView *scrollView = [[UIScrollView alloc]initWithFrame:screenRect];
+    [self.window addSubview:scrollView];
     
-    [self.window addSubview:firstView];
+    // Create a super-sized HypnosisView and add it to the scrollview
+    BNRHypnosisView * hypnosisView = [[BNRHypnosisView alloc]initWithFrame:bigRect];
+    [scrollView addSubview:hypnosisView];
+    
+    // Tell the scrollView how big its content area is
+    scrollView.contentSize = bigRect.size;
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
